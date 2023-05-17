e67d2db905be1f04b013e82aeb92ba228017ac59
Robin Kuijt <robin.kuijt@hu.nl>
2023-05-17 18:19:44 +0200

    fix: Only set credentials path if not set yet.

--- do-upload.sh
+++ do-upload.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-export GOOGLE_APPLICATION_CREDENTIALS=/app/config/credentials.json
+if [ -z ${GOOGLE_APPLICATION_CREDENTIALS+x} ]; then
+  export GOOGLE_APPLICATION_CREDENTIALS=/app/config/credentials.json
+fi
 gupload --to $FOLDER_ID /app/upload/*
